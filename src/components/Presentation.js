e581635cdcbc283ea746aadb8c9a1aa0272f98dc
AlexDesout <alexdesout@outlook.fr>
2024-02-17 17:11:00 +0100

    first commit

---
+++ src/components/Presentation.js
@@ -0,0 +1,11 @@
+
+
+const Presentation = () => {
+    return (
+        <div className="presentation">
+            <h2 className="dark-text">Bonjour</h2>
+        </div>
+    );
+}
+
+export default Presentation;
